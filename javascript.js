de30eb9eb29001effda4611ebbd5929e55a6108a
miiv0 <minzana2002@gmail.com>
2025-03-27 00:57:01 -0400

    add player choice, returns rps if 1 2 or 3

--- javascript.js
+++ javascript.js
@@ -11,3 +11,17 @@ function getComputerChoice(max) {               // gets a random number from (0,
     return ("Scissors");
 }
 console.log(getComputerChoice(3))
+
+function getHumanChoice() {
+    const result = prompt("Choose Rock(1), Paper(2), or Scissors(3)")
+    if (result === "1") {
+        return ("Rock")
+    } else if (result === "2") {
+        return ("Paper")
+    } else if (result === "3") {
+        return ("Scissors")
+    } else if (result >= "4")
+        getHumanChoice()
+}
+console.log(getHumanChoice())
+
